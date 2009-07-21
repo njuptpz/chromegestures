9b1ef9b6c0b3dd2f972985f5744c17cc4f4e9eb9
Kryptyx <Kryptyx@61c6bb36-725c-11de-a7f0-e7cb3d96a278>
2009-07-21 20:16:00 +0000

    Complete revamp on how we define gestures. This should make it easier for everyone to modify.

--- trunk/ChromeGestures.user.js
+++ trunk/ChromeGestures.user.js
@@ -1,93 +1,68 @@
-/** READ THIS BEFORE EDITING **/
-//  Here you can create your own gestures. For the moment, how this works is that you create
-//  a Gesture and an Action. 
-//
-//         _____________
-//   _____/ Terminology |___________________________________________________________________________________________________________________________
-//  |                                                                                                                                               |
-//  |    Moves are either 'up', 'down', 'left', 'right' or 'click'.                                                                                 |
-//  |                                                                                                                                               |
-//  |    A Gesture is an array of Moves e.g.: myNewGesture = ["up","down","left"].                                                                  |
-//  |        - You should never have a Gesture that has two identical consecutive moves. e.g.: ["left","right","right"] is not a valid Gesture.     |
-//  |                                                                                                                                               |
-//  |    An Action is a function that takes no parameters and has no return value. e.g.: myNewAction = function(){ alert("OMG!") }                  |
-//  |                                                                                                                                               |
-//  |    You can then configure ChromeGesture by modifying the myGestures array.                                                                    |
-//  |    myGestures is an array where the index is your Gesture and the data is your Action. e.g.: myGestures[myNewGesture] = myNewAction;          |
-//  |       - Do not use the same Gesture for two Actions.                                                                                          |
-//  |                                                                                                                                               |
-//   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-// 
-//  AUTHORS: Kyrax, Waha, Kryptyx
-//
-// In case of doubt, drop by the forums on chromeplugins.org and ask. We'll answer ;)
-var us = (typeof(chrome.extension) == "undefined") // [Waha] DO NOT EDIT
-var gesturesHelp = new Array(); // [Waha] DO NOT EDIT
-// ============ BEGIN STUFF YOU CAN (AND SHOULD) EDIT ============
-var popupLoader = false; //If you want your options to load, enable this. If it gives you trouble, disable it.
+// ======================================================== //
+//	Script: ChromeGestures.user.js
+//	Authors: Kyrax, Waha, Kryptyx
+//	Website: http://code.google.com/p/chromegestures
+// ======================================================== //
 
-//[Waha] This is in quotes, because in some editors, comments are minified, and not fixed width. :]
-" _____ ______  _____ _______ _    _ _____  ______  _____  \
- / ____|  ____|/ ____|__   __| |  | |  __ \|  ____|/ ____| \
-| |  __| |__  | (___    | |  | |  | | |__) | |__  | (___   \
-| | |_ |  __|  \___ \   | |  | |  | |  _  /|  __|  \___ \  \
-| |__| | |____ ____) |  | |  | |__| | | \ \| |____ ____) | \
- \_____|______|_____/   |_|   \____/|_|  \_\______|_____/  \
-" //Add your getures here! Make sure the name and the gestures themselves are unique!
-var newTabGesture           = ["down","right"];
-var blankTabGesture         = ["down","right","down"];
-var closeTabGesture         = ["down","left"];
-var historyBackGesture      = ["left"];
-var historyForwardGesture   = ["right"];
-var hidePageGesture         = ["click","click"];
-var refreshGesture          = ["right","down","left","up","right"];
-var refreshGesture2         = ["up","right","down","left","up"];
-var refreshGesture3         = ["up","right","down","left","up","right"];
-var helpGesture             = ["up","right","down","left","down"];
-if(!us)var optionsGesture   = ["up","down"];
-var searchGesture           = ["up"];
-var testGesture             = ["left","click","right"];
+var us = (typeof(chrome.extension) == "undefined") // DO NOT EDIT
+var gesturesHelp = new Array(); // DO NOT EDIT
+var myGestures = new Array(); // DO NOT EDIT
+var isHiding = false; // DO NOT EDIT
+var bgImage = null; // DO NOT EDIT
 
-// [Kryptyx] Change the default URL for a newTabGesture
-var newTabURL               = "http://google.com";
+var popupLoader = false; // Load options through popup window, true or false. If false options will not load.
 
-// [Kyrax] TODO: Support for Chrome API
-"         _____ _______ _____ ____  _   _  _____  \
-    /\   / ____|__   __|_   _/ __ \| \ | |/ ____| \
-   /  \ | |       | |    | || |  | |  \| | (___   \
-  / /\ \| |       | |    | || |  | | . ` |\___ \  \
- / ____ \ |____   | |   _| || |__| | |\  |____) | \
-/_/    \_\_____|  |_|  |_____\____/|_| \_|_____/  \
-" /* Add the actions for your gestures here. This is just regular JavaScript.
- Support for Chrome API will be added when communication is back. */
-function  newTabAction() {
+// [Kryptyx] Revamped the way we define gestures, to define a new gesture simply call the newGesture function with the correct paramaters.
+// newGesture("moves", action, "description", "coords");
+// Moves = The pattern needed to trigger the gesture, seperate each move with a space. (ex. "left right click down")
+// Action = The action that the gesture triggers, do not use quotes.
+// Description = The description of the gesture for the help page, this is optional.
+// Coords = The coords to draw the gesture on the help page, this is optional.
+
+newGesture("down right", newTab, "Open a new tab. You can set the homepage in options.", "20,20,60,60");
+newGesture("down right down", blankTab, "Open a blank tab.", "35,20,30,30,30");
+newGesture("down left", closeTab, "Closes the current tab.", "80,20,60,60");
+newGesture("left", historyBack, "Goes Back to the last visited page.", "80,50,60");
+newGesture("right", historyForward, "Goes Forward to the next visited page.", "20,50,60");
+newGesture("click click", hidePage, "Hide the current page.<br/>Use again to display it.", "50,50,10,6");
+newGesture("right down left up right", refreshPage, "Refreshes the current page.", "20,20,60,60,60,50,50");
+newGesture("up right down left up", refreshPage);
+newGesture("up right down left up right", refreshPage);
+newGesture("up right down left down", helpBox, "Opens the Chrome Gestures help file (this).", "20,50,30,60,30,30,30");
+if(!us)newGesture("up down", optionsBox, "Opens the Chrome Gestures options.", "50,70,50,60");
+newGesture("up", newSearch, "Search the selected text.", "50,80,60");
+newGesture("left click right", runTest);
+
+var newTabURL = "http://google.com"; // Default newTab page, feel free to change.
+
+// DEFAULT ACTIONS
+
+function  newTab() {
 	window.open(newTabURL);
 }
 
-function  blankTabAction() {
+function  blankTab() {
 	window.open("about:blank");
 }
 
-function closeTabAction() {
+function closeTab() {
 	window.open('', '_self');
     self.close();
 }
 
-function historyBackAction() {
+function historyBack() {
     history.back();
 }
 
-function historyForwardAction() {
+function historyForward() {
     history.forward();
 }
 
-function refreshAction() {
+function refreshPage() {
 	location.reload();
 }
 
-var isHiding = false;
-var bgImage = null;
-function hidePageAction() {
+function hidePage() {
 	if(isHiding){
 		document.body.style.backgroundImage = bgImage;
 		document.body.style.display = "block";
@@ -100,13 +75,13 @@ function hidePageAction() {
 	}
 }
 
-function optionsAction(){
+function optionsBox() {
 	window.open("chrome-extension://"+chrome.extension.id_+"/options.html","_blank",
 		"directories=no,height=210,width=500,left=10,top=10,location=no,"+
 		"menubar=no,scrollbars=no,status=no,titlebar=no,toolbar=no");
 }
 
-function helpAction(){
+function helpBox() {
 	var win = window.open("about:blank","_blank",
 		"directories=no,height=500,width=500,left=10,top=10,location=no,"+
 		"menubar=no,scrollbars=no,status=no,titlebar=no,toolbar=no");
@@ -130,10 +105,8 @@ function helpAction(){
 			"<td style=\"height:76px;vertical-align:top;"+moar+"\">"+gesturesHelp[i].d+"</td></tr>"+
 			"<tr><td style=\"border-top:2px solid #000000;height:20px;\"><b>"+g+"</b></td></tr>";
 	}
-	/* [Waha] Aw come on..two seperate loops? really? D:
-	I had to do this cause adjusting the table's innerHTML was removing the canvas content...*/
+	/* [Waha] Canvas Hack */
 	for(var i in gesturesHelp){
-		//Draw gesture
 		var tc = win.document.getElementById("ges"+i).getContext('2d');
 		tc.beginPath();
 		var lastx=gesturesHelp[i].x,lasty=gesturesHelp[i].y;
@@ -155,12 +128,13 @@ function helpAction(){
 		tc.stroke();
 	}
 }
+
 function isURL(str) {
 	var regexp = /(http:\/\/|w{3}\.)(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 	return regexp.test(str);
 }
-function searchAction()
-{
+
+function newSearch(){
 	var selectedTxt = '';
 	if (window.getSelection){
 		selectedTxt = window.getSelection().toString();
@@ -184,57 +158,11 @@ function searchAction()
 	}
 }
 
-function testAction(){
+function runTest(){
 	alert(ChromeGesture.MINIMUM_DELTAY);
 }
 
-"_____  ______ _            _______ _____ ____  _   _  _____  \
-|  __ \|  ____| |        /\|__   __|_   _/ __ \| \ | |/ ____| \
-| |__) | |__  | |       /  \  | |    | || |  | |  \| | (___   \
-|  _  /|  __| | |      / /\ \ | |    | || |  | | . ` |\___ \  \
-| | \ \| |____| |____ / ____ \| |   _| || |__| | |\  |____) | \
-|_|  \_\______|______/_/    \_\_|  |_____\____/|_| \_|_____/  \
-" /*This is where you link your gesture and action together.
-As you can see, the syntax is simply: myGestures[yourGesture] = yourAction;*/
-var myGestures = [];
-myGestures[newTabGesture]           = newTabAction;
-myGestures[blankTabGesture]         = blankTabAction;
-myGestures[closeTabGesture]         = closeTabAction;
-myGestures[historyBackGesture]      = historyBackAction;
-myGestures[historyForwardGesture]   = historyForwardAction;
-myGestures[hidePageGesture]         = hidePageAction;
-myGestures[refreshGesture]          = refreshAction;
-myGestures[refreshGesture2]         = refreshAction;
-myGestures[refreshGesture3]         = refreshAction;
-if(!us)myGestures[optionsGesture]   = optionsAction;
-myGestures[helpGesture]             = helpAction;
-myGestures[searchGesture]           = searchAction;
-myGestures[testGesture]             = testAction;
-
-"_    _ ______ _      _____   \
-| |  | |  ____| |    |  __ \  \
-| |__| | |__  | |    | |__) | \
-|  __  |  __| | |    |  ___/  \
-| |  | | |____| |____| |      \
-|_|  |_|______|______|_|      \
-" /* Register to be in the Help File here.
-Supply with ID, Description, Starting X position in drawing, Starting Y position in drawing, and the offsets in pixels from the last point according to the gestures
-For instance, if you start at [20,20] and your gestures are ["right", "down"], and you supply it with 30,40 as the offsets. It will draw a line from
-[20,20] to [50,20] to [50,60]. Because it went 30 to the right of the origin, and then 40 down from the new position.
-If the gesture is a click, you supply the radius of the circle instead of an offset.
-You are limited to a 100x100 pixel canvas. */
-registerForHelp(newTabGesture,"Open a new tab. You can set the homepage in options.",20,20,60,60);
-registerForHelp(blankTabGesture,"Open a blank tab.",35,20,30,30,30);
-registerForHelp(closeTabGesture,"Closes the current tab.",80,20,60,60);
-registerForHelp(historyBackGesture,"Goes Back to the last visited page.",80,50,60);
-registerForHelp(historyForwardGesture,"Goes Forward to the next visited page.",20,50,60);
-registerForHelp(hidePageGesture,"Hide the current page.<br/>Use again to display it.",50,50,10,6);
-registerForHelp(refreshGesture,"Refreshes the current page.<br/>Can also start with an upward motion instead of right.",20,20,60,60,60,50,50);
-if(!us)registerForHelp(optionsGesture,"Opens the Chrome Gestures options.",50,70,50,60);
-registerForHelp(helpGesture,"Opens the Chrome Gestures help file (this).",20,50,30,60,30,30,30);
-registerForHelp(searchGesture,"Search the selected text.",50,80,60);
-
-// ============ UNLESS YOU KNOW WHAT YOU ARE DOING, DON'T EDIT PAST HERE ============
+// ============ UNLESS YOU KNOW WHAT YOU ARE DOING, DON'T EDIT PAST HERE ============ //
 
 var clickPast;
 var clickNow;
@@ -243,16 +171,31 @@ var wind;
 var alpha=1.0;
 var logging = false;
 
+function newGesture(moves, action, description, coords){
+	moves=moves.replace(/ /g, ",");
+	moves=moves.split(",");
+	if (arguments.length > 2) {
+		registerForHelp(moves, description, coords);
+	}
+	return myGestures[moves] = action;
+}
+
 function log(txt){if(logging){console.log(txt);}}
 function warn(txt){if(logging){console.warn(txt);}}
 function error(txt){if(logging){console.error(txt);}}
 function debug(txt){if(logging){console.debug(txt);}}
 
-function registerForHelp(gestures,desc,startx,starty){
-	if(arguments.length-4 != gestures.length){
+function registerForHelp(gestures,desc,coords){
+
+	coords=coords.split(",");
+	var startx=coords[0];
+	var starty=coords[1];
+	
+	if(coords.length-2 != gestures.length){
 		alert("Error registering help for \""+gestures+"\": Not enough offsets given.");
 		return;
 	}
+	
 	var i = gesturesHelp.length;
 	gesturesHelp[i] = new Object();
 	gesturesHelp[i].d = desc;
@@ -261,16 +204,17 @@ function registerForHelp(gestures,desc,startx,starty){
 	gesturesHelp[i].y = starty;
 	gesturesHelp[i].pnts = new Array();
 	for(var j=0;j<gestures.length;j++){
-		if(gestures[j] == "click")gesturesHelp[i].pnts[j] = new Array("c",arguments[j+4]);
+		if(gestures[j] == "click")gesturesHelp[i].pnts[j] = new Array("c",coords[j+2]);
 		else{
-			if(gestures[j] == "left")startx -= arguments[j+4];
-			else if(gestures[j] == "right")startx += arguments[j+4];
-			else if(gestures[j] == "up")starty -= arguments[j+4];
-			else if(gestures[j] == "down")starty += arguments[j+4];
+			if(gestures[j] == "left")startx -= coords[j+2];
+			else if(gestures[j] == "right")startx += coords[j+2];
+			else if(gestures[j] == "up")starty -= coords[j+2];
+			else if(gestures[j] == "down")starty += coords[j+2];
 			gesturesHelp[i].pnts[j] = new Array(startx,starty);
 		}
 	}
 }
+
 
 function getData(ntpurl,delta,captime,clickto){
 	newTabURL = ntpurl;
@@ -288,6 +232,7 @@ function getData(ntpurl,delta,captime,clickto){
 if(!us)contentWindow.CHROMEGESTURES_setData = getData;
 
 // LOAD SAVED DATA
+
 if(!us){
 	if(popupLoader){
 		contentWindow.CHROMEGESTURES_loadData = true;
@@ -324,16 +269,14 @@ function hideGesture(){
 	}
 }
 
-// CONSTANTS
-
 var ChromeGesture = {
-
+    // SETTINGS
     MINIMUM_DELTAY : 10,			// tweak this - Minimum Y distance that needs to be travelled between 2 captures for a move to be detected
     MINIMUM_DELTAX : 10,			// tweak this - Same for X
     DELAY_BETWEEN_CAPTURES : 10, 	// tweak this - Time between captures
 	CLICK_TIMEOUT : 1000,			// tweak this - This is the timeout period between clicks (1000 = 1 second)
 	
-	//Globals
+	// GLOBALS
     LEFT_CLICK : 1,					// Number associated with Left Click
     RIGHT_CLICK : 3,				// Number associated with Right Click
     pastX : 0,
@@ -341,7 +284,7 @@ var ChromeGesture = {
     currentGesture : [],
     pastTime : new Date(),
 
-	// Functions
+	// FUNCTIONS
     captureGesture : function(event) {
         
         var currentTime = new Date();
